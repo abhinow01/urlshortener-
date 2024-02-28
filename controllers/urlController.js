ddfbe3b2183fa4ae1d0db86d76bf55e84ad588e1
abhinav khanna <kabhinav99@gmail.com>
2024-02-28 21:39:14 +0530

    fix2

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -2,18 +2,23 @@ const shortid = require('shortid');
 const Url = require('../models/urlModel');
 
 const shortenUrl = async (req, res) => {
+  try {
   const { originalUrl } = req.body;
   
   if(!originalUrl){
     return res.status(400).json({error:'original url is required'});
   }
 
-  try {
-    const shortUrl =   shortid.generate();
-    const createdUrl =  await Url.create({originalUrl , shortUrl });
-     
-res.status(200).json(createdUrl);
+  let url = await Url.findOne({originalUrl});
 
+     if(!url){
+  
+    const shortUrl =  shortid.generate();
+    url =  await Url.create({originalUrl , shortUrl });
+     
+
+     }
+     res.status(200).json(createdUrl);
   } catch (error) {
     res.status(500).json({ message: 'Internal server error', error });
     console.log(error);
